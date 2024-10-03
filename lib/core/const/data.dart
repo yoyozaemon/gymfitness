8d410211873a771a853dc2ed224c7b9e968ccf4c
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2024-10-03 11:54:32 +0530

    stage 1 code added

---
+++ lib/core/const/data.dart
@@ -0,0 +1,28 @@
+import 'package:gymfitness/core/const/path.dart';
+import 'package:gymfitness/core/const/text.dart';
+
+class DataConstants {
+  // Onboarding
+  static final onboardingTiles = [
+    OnboardingTile(
+      title: TextConstants.onboarding1Title,
+      mainText: TextConstants.onboarding1Description,
+      imagePath: PathConstants.onboarding1,
+    ),
+    OnboardingTile(
+      title: TextConstants.onboarding2Title,
+      mainText: TextConstants.onboarding2Description,
+      imagePath: PathConstants.onboarding2,
+    ),
+    OnboardingTile(
+      title: TextConstants.onboarding3Title,
+      mainText: TextConstants.onboarding3Description,
+      imagePath: PathConstants.onboarding3,
+    ),
+  ];
+
+  static OnboardingTile(
+      {required String title,
+      required String mainText,
+      required String imagePath}) {}
+}
